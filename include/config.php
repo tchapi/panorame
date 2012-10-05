8d065c5436c3f1575f3aa954e012e1d5e2e5f6fd
tchap <regbasket@gmail.com>
2012-10-05 16:19:20 +0300

    Add earth radius in config.php

--- include/config.php
+++ include/config.php
@@ -8,42 +8,44 @@
   define('_closestPointRadius_search', 200);
   define('_closestPointRadius_edit', 10);
   define('_extendBoundsPointRadius', 500);
+  
+  define('_earth_radius', 6371030.00); // in m
+  
+  if ($app == '_FRONTEND') {
 
-	if ($app == '_FRONTEND') {
+    /*
+     * FRONTEND Defaults
+     */
 
-		/*
-		 * FRONTEND Defaults
-		 */
+    $addedScript = "";
+    $framework   = "gmaps";
+    $provider    = "";
+    $editMode    = false;
 
-		$addedScript = "";
-		$framework   = "gmaps";
-		$provider    = "";
-		$editMode    = false;
+  } elseif ($app == '_BACKEND') {
+    
+    /*
+     * BACKEND Defaults
+     */
 
-	} elseif ($app == '_BACKEND') {
-		
-		/*
-		 * BACKEND Defaults
-		 */
+    $server   = "localhost";
+    $user     = "isocron"; // tchap_panorame
+    $password = "isocron"; // 8y3nP9922z6tu2en
+    $database = "isocron"; // tchap_panorame
 
-		$server   = "localhost";
-		$user     = "isocron"; // tchap_panorame
-		$password = "isocron"; // 8y3nP9922z6tu2en
-		$database = "isocron"; // tchap_panorame
+    $engine   = "mysql"; // OR mysql
 
-		$engine   = "mysql"; // OR mysql
+    global $DBConnection;
 
-		global $DBConnection;
+    /* ----------------------------- */
+    /* ------- DO NOT MODIFY ------- */
 
-		/* ----------------------------- */
-		/* ------- DO NOT MODIFY ------- */
+      $database_connector_path = _PATH.'DB/'.$engine.'/DBConnector.php';
+      $database_utils_path = _PATH.'DB/'.$engine.'/DBUtils.php';
 
-			$database_connector_path = _PATH.'DB/'.$engine.'/DBConnector.php';
-			$database_utils_path = _PATH.'DB/'.$engine.'/DBUtils.php';
+      include_once($database_connector_path);
+      include_once($database_utils_path);
 
-			include_once($database_connector_path);
-			include_once($database_utils_path);
+    /* ----------------------------- */
 
-		/* ----------------------------- */
-
-	}
+  }
