b78c058042cefb4d94f20e1e37c3a43244daf556
tchap <regbasket@gmail.com>
2012-10-05 16:11:30 +0300

    Update for action summary in admin
    and indentation

--- include/actions/edit/cutEdge.action.php
+++ include/actions/edit/cutEdge.action.php
@@ -1,41 +1,38 @@
 <?php
 
-	$app = '_BACKEND';
-	include_once('../../config.php');
+  $app = '_BACKEND';
+  include_once('../../config.php');
 
-	if (isset($_POST['start_id']) && isset($_POST['dest_id']) && isset($_POST['new_lat']) && isset($_POST['new_lng']) && isset($_POST['new_alt']) && isset($_POST['edge_id'])  &&  0 != $_POST['edge_id']){
+  if (isset($_POST['start_id']) && isset($_POST['dest_id']) && isset($_POST['new_lat']) && isset($_POST['new_lng']) && isset($_POST['new_alt']) && isset($_POST['edge_id'])  &&  0 != $_POST['edge_id']){
 
-		$start_id = intval($_POST['start_id']);
-		$dest_id = intval($_POST['dest_id']);
-		$new_lat = floatval($_POST['new_lat']);
-		$new_lng = floatval($_POST['new_lng']);
-		$edge_id = intval($_POST['edge_id']);
+    $start_id = intval($_POST['start_id']);
+    $dest_id  = intval($_POST['dest_id']);
+    
+    $new_lat  = floatval($_POST['new_lat']);
+    $new_lng  = floatval($_POST['new_lng']);
+    $new_alt  = floatval($_POST['new_alt']);
 
-		$result = Utils::cutEdge($start_id, $dest_id, $new_lat, $new_lng, $new_alt, $edge_id);
+    $edge_id  = intval($_POST['edge_id']);
 
-		if ($result == null || $result == false){
+    $result = Utils::cutEdge($start_id, $dest_id, $new_lat, $new_lng, $new_alt, $edge_id);
 
-			$result = array(
-				'error' => 'Error updating vertex',
-				'code'	=> 2
-			);
+    if ($result == null || $result == false){
 
-		} else {
+      $result = array(
+        'error' => 'Error updating vertex',
+        'code'  => 2
+      );
 
-			$result = array(
-				'status' => $result
-			);
+    }
 
-		}
+  } else {
+  
+    $result = array(
+      'error' => 'Bad Request',
+      'code'  => 1
+    );
+    
+  }
 
-	} else {
-	
-		$result = array(
-			'error' => 'Bad Request',
-			'code'	=> 1
-		);
-		
-	}
-
-	header('Content-type: application/json');
-	print json_encode($result);
+  header('Content-type: application/json');
+  print json_encode($result);
