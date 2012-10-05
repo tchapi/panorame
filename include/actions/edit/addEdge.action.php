39bd902ccc48fb69f09cd0515006ac3696374c6b
tchap <regbasket@gmail.com>
2012-10-05 16:10:13 +0300

    Update to reflect action summary changes
    ... and indentation

--- include/actions/edit/addEdge.action.php
+++ include/actions/edit/addEdge.action.php
@@ -1,58 +1,39 @@
 <?php
 
-	$app = '_BACKEND';
-	include_once('../../config.php');
+  $app = '_BACKEND';
+  include_once('../../config.php');
 
-	if (isset($_POST['start_lat']) && isset($_POST['dest_lat']) && isset($_POST['start_lng']) && isset($_POST['dest_lng'])){
+  if (isset($_POST['start_lat']) && isset($_POST['dest_lat']) && isset($_POST['start_lng']) && isset($_POST['dest_lng'])){
 
-		$start_lat = floatval($_POST['start_lat']);
-		$start_lng = floatval($_POST['start_lng']);
-		$start_alt = isset($_POST['start_alt'])?intval($_POST['start_alt']):0;
+    $start_lat = floatval($_POST['start_lat']);
+    $start_lng = floatval($_POST['start_lng']);
+    $start_alt = isset($_POST['start_alt'])?intval($_POST['start_alt']):0;
 
-		$dest_lat = floatval($_POST['dest_lat']);
-		$dest_lng = floatval($_POST['dest_lng']);
-		$dest_alt = isset($_POST['dest_alt'])?intval($_POST['dest_alt']):0;
+    $dest_lat = floatval($_POST['dest_lat']);
+    $dest_lng = floatval($_POST['dest_lng']);
+    $dest_alt = isset($_POST['dest_alt'])?intval($_POST['dest_alt']):0;
 
-		$type = isset($_POST['type'])?intval($_POST['type']):0;
+    $type = isset($_POST['type'])?intval($_POST['type']):1;
 
-		$result = Utils::addEdge($start_lat, $start_lng, $start_alt, $dest_lat, $dest_lng, $dest_alt, $type);
-	
-		if ($result == null || $result == false){
+    $result = Utils::addEdge($start_lat, $start_lng, $start_alt, $dest_lat, $dest_lng, $dest_alt, $type);
+  
+    if ($result == null || $result == false){
 
-			$result = array(
-				'error' => 'Error creating edge',
-				'code'	=> 2
-			);
+      $result = array(
+        'error' => 'Error creating edge',
+        'code'  => 2
+      );
 
-		} else {
+    }
 
-			$result = array(
-				'request' => array(
-					'start' => array(
-						'lat' => $start_lat,
-						'lng' => $start_lng,
-						'alt' => $start_alt
-						),
-					'dest' => array(
-						'lat' => $dest_lat,
-						'lng' => $dest_lng,
-						'alt' => $dest_alt
-						),
-					'type' => $type,
-					),
-				'new'  => $result
-			);
+  } else {
+  
+    $result = array(
+      'error' => 'Bad Request',
+      'code'  => 1
+    );
+    
+  }
 
-		}
-
-	} else {
-	
-		$result = array(
-			'error' => 'Bad Request',
-			'code'	=> 1
-		);
-		
-	}
-
-	header('Content-type: application/json');
-	print json_encode($result);
+  header('Content-type: application/json');
+  print json_encode($result);
