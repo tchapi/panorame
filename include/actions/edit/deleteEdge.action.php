e2dcff41b1b33af967a79bda58bb1cffca0ca391
tchap <regbasket@gmail.com>
2012-10-05 16:12:16 +0300

    Update for action summary
    and indentation

--- include/actions/edit/deleteEdge.action.php
+++ include/actions/edit/deleteEdge.action.php
@@ -1,37 +1,31 @@
 <?php
 
-	$app = '_BACKEND';
-	include_once('../../config.php');
+  $app = '_BACKEND';
+  include_once('../../config.php');
 
-	if (isset($_POST['edge_id'])){
+  if (isset($_POST['edge_id'])){
 
-		$edge_id = intval($_POST['edge_id']);
+    $edge_id = intval($_POST['edge_id']);
 
-		$result = Utils::deleteEdge($edge_id);
-	
-		if ($result == null || $result == false){
+    $result = Utils::deleteEdge($edge_id);
+  
+    if ($result == null || $result == false){
 
-			$result = array(
-				'error' => 'Error deleting edge',
-				'code'	=> 2
-			);
+      $result = array(
+        'error' => 'Error deleting edge',
+        'code'  => 2
+      );
 
-		} else {
+    }
 
-			$result = array(
-				'status' => 'ok'
-			);
+  } else {
+  
+    $result = array(
+      'error' => 'Bad Request',
+      'code'  => 1
+    );
+    
+  }
 
-		}
-
-	} else {
-	
-		$result = array(
-			'error' => 'Bad Request',
-			'code'	=> 1
-		);
-		
-	}
-
-	header('Content-type: application/json');
-	print json_encode($result);
+  header('Content-type: application/json');
+  print json_encode($result);
