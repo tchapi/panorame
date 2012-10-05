5426f5de68e7afaeb67daa746f6bf76f9c4392f1
tchap <regbasket@gmail.com>
2012-10-05 16:37:35 +0300

    Improve cases and indentation

--- include/actions/getObjectsIn.action.php
+++ include/actions/getObjectsIn.action.php
@@ -1,58 +1,55 @@
 <?php
 
-	$app = '_BACKEND';
-	include_once('../config.php');
+  $app = '_BACKEND';
+  include_once('../config.php');
 
-	// Defaults to "Edges"
+  if (isset($_POST['bounds']) && isset($_POST['poi']) && isset($_POST['type']){
 
-	if (isset($_POST['type']) && $_POST['type'] == 'vertices'){
-		$type = 'vertices';
-	} elseif (isset($_POST['type']) && $_POST['type'] == 'edges') {
-		$type = 'edges';
-	} else if (isset($_POST['type']) && $_POST['type'] == 'tree'){
-		$type = 'tree';
-	}
+    switch ($_POST['type']){
+      case 'vertices':
+      case 'edges':
+      case 'tree':
+        $type = $_POST['type'];
+        break;
+      default:
+        $type = 'vertices';
+    }
 
-	if (isset($_POST['bounds']) && isset($_POST['poi'])){
+    $bounds = $_POST['bounds'];
 
-		$bounds = $_POST['bounds'];
+    // Get POI Closest point
+    $poi = array('lat' => $_POST['poi']['lat'], 'lng' => $_POST['poi']['lng']);
+    $closestVertex = Utils::getClosestVertex($poi['lat'], $poi['lng'], _closestPointRadius_search);
 
-		// Get POI Closest point
+    // Get objects
+    if ($type == 'vertices'){
 
-		$poi = array('lat' => $_POST['poi']['lat'], 'lng' => $_POST['poi']['lng']);
-		$closestVertex = Utils::getClosestVertex($poi['lat'], $poi['lng'], _closestPointRadius_search);
+      $objects = Utils::getVerticesIn($bounds["NW_lat"], $bounds["NW_lng"], $bounds["SE_lat"], $bounds["SE_lng"], $closestVertex['point']['lat'], $closestVertex['point']['lng']);
+    
+    } elseif ($type == 'edges'){
 
-		// Get objects
-		if ($type == 'vertices'){
+      $objects = Utils::getEdgesIn($bounds["NW_lat"], $bounds["NW_lng"], $bounds["SE_lat"], $bounds["SE_lng"], $closestVertex['point']['lat'], $closestVertex['point']['lng']);
 
-			$objects = Utils::getVerticesIn($bounds["NW_lat"], $bounds["NW_lng"], $bounds["SE_lat"], $bounds["SE_lng"], $closestVertex['point']['lat'], $closestVertex['point']['lng']);
-		
-		} elseif ($type == 'edges'){
+    } elseif ($type == 'tree'){
 
-			$objects = Utils::getEdgesIn($bounds["NW_lat"], $bounds["NW_lng"], $bounds["SE_lat"], $bounds["SE_lng"], $closestVertex['point']['lat'], $closestVertex['point']['lng']);
+      $objects = Utils::getVerticesAndChildrenIn($bounds["NW_lat"], $bounds["NW_lng"], $bounds["SE_lat"], $bounds["SE_lng"], $closestVertex['point']['lat'], $closestVertex['point']['lng']);
 
-		} elseif ($type == 'tree'){
+    }
 
-			$objects = Utils::getVerticesAndChildrenIn($bounds["NW_lat"], $bounds["NW_lng"], $bounds["SE_lat"], $bounds["SE_lng"], $closestVertex['point']['lat'], $closestVertex['point']['lng']);
+    $result = array(
+      'closest' => $closestVertex,
+      'count'   => count($objects),
+      $type     => $objects
+    );
 
-		}
+  } else {
 
-		$result = array(
-		//	'bounds' => $bounds,
-		//	'poi'	 => $poi,
-			'closest'=> $closestVertex,
-			'count'  => count($objects),
-			  $type  => $objects
-		);
+    $result = array(
+      'error' => 'Bad Request',
+      'code'  => 1
+    );
+    
+  }
 
-	} else {
-
-		$result = array(
-			'error' => 'Bad Request',
-			'code'	=> 1
-		);
-		
-	}
-
-	header('Content-type: application/json');
-	print json_encode($result);
+  header('Content-type: application/json');
+  print json_encode($result);
