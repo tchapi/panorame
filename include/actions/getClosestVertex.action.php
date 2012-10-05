67c06596e1b9d35589fb8c8871f4862112c2c482
tchap <regbasket@gmail.com>
2012-10-05 16:40:12 +0300

    Update include/actions/getClosestVertex.action.php

--- include/actions/getClosestVertex.action.php
+++ include/actions/getClosestVertex.action.php
@@ -1,25 +1,25 @@
 <?php
 
-	$app = '_BACKEND';
-	include_once('../config.php');
+  $app = '_BACKEND';
+  include_once('../config.php');
 
-	if (isset($_POST['lat']) && isset($_POST['lng']) ){
+  if (isset($_POST['lat']) && isset($_POST['lng']) ){
 
-		$lat = floatval($_POST['lat']);
-		$lon = floatval($_POST['lng']);
+    $lat = floatval($_POST['lat']);
+    $lon = floatval($_POST['lng']);
 
-		$radius = isset($_POST['radius'])?intval($_POST['radius']):_closestPointRadius_search;
+    $radius = isset($_POST['radius'])?intval($_POST['radius']):_closestPointRadius_search;
 
-		$result = Utils::getClosestVertex($lat, $lon, $radius);
-	
-	} else {
-	
-		$result = array(
-			'error' => 'Bad Request',
-			'code'	=> 1
-		);
-		
-	}
+    $result = Utils::getClosestVertex($lat, $lon, $radius);
+  
+  } else {
+  
+    $result = array(
+      'error' => 'Bad Request',
+      'code'  => 1
+    );
+    
+  }
 
-	header('Content-type: application/json');
-	print json_encode($result);
+  header('Content-type: application/json');
+  print json_encode($result);
