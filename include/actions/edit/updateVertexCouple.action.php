2b970564dc21c3d3d3b36e80a8bd2aea6caa2e30
tchap <regbasket@gmail.com>
2012-10-05 16:14:22 +0300

    Update for action summary in admin

--- include/actions/edit/updateVertexCouple.action.php
+++ include/actions/edit/updateVertexCouple.action.php
@@ -1,48 +1,41 @@
 <?php
 
-	$app = '_BACKEND';
-	include_once('../../config.php');
+  $app = '_BACKEND';
+  include_once('../../config.php');
 
-	if (isset($_POST['start_lat']) && isset($_POST['start_lng']) && isset($_POST['start_id']) && isset($_POST['dest_lat']) && isset($_POST['dest_lng']) && isset($_POST['dest_id']) && isset($_POST['edge_id'])){
+  if (isset($_POST['start_lat']) && isset($_POST['start_lng']) && isset($_POST['start_id']) && isset($_POST['dest_lat']) && isset($_POST['dest_lng']) && isset($_POST['dest_id']) && isset($_POST['edge_id'])){
 
-		$start_lat = floatval($_POST['start_lat']);
-		$start_lng = floatval($_POST['start_lng']);
-		$start_id = intval($_POST['start_id']);
-		$start_alt = intval($_POST['start_alt']);
+    $start_lat = floatval($_POST['start_lat']);
+    $start_lng = floatval($_POST['start_lng']);
+    $start_id = intval($_POST['start_id']);
+    $start_alt = intval($_POST['start_alt']);
 
+    $dest_lat = floatval($_POST['dest_lat']);
+    $dest_lng = floatval($_POST['dest_lng']);
+    $dest_id = intval($_POST['dest_id']);
+    $dest_alt = intval($_POST['dest_alt']);
 
-		$dest_lat = floatval($_POST['dest_lat']);
-		$dest_lng = floatval($_POST['dest_lng']);
-		$dest_id = intval($_POST['dest_id']);
-		$dest_alt = intval($_POST['dest_alt']);
+    $edge_id = intval($_POST['edge_id']);
 
-		$edge_id = intval($_POST['edge_id']);
+    $result = Utils::updateVertexCouple($start_id, $start_lat, $start_lng, $start_alt, $dest_id, $dest_lat, $dest_lng, $dest_alt, $edge_id);
+  
+    if ($result == null || $result == false){
 
-		$result = Utils::updateVertexCouple($start_id, $start_lat, $start_lng, $start_alt, $dest_id, $dest_lat, $dest_lng, $dest_alt, $edge_id);
-	
-		if ($result == null || $result == false){
+      $result = array(
+        'error' => 'Error updating vertices',
+        'code'  => 2
+      );
 
-			$result = array(
-				'error' => 'Error updating vertex',
-				'code'	=> 2
-			);
+    }
 
-		} else {
+  } else {
+  
+    $result = array(
+      'error' => 'Bad Request',
+      'code'  => 1
+    );
+    
+  }
 
-			$result = array(
-				'status' => $result
-			);
-
-		}
-
-	} else {
-	
-		$result = array(
-			'error' => 'Bad Request',
-			'code'	=> 1
-		);
-		
-	}
-
-	header('Content-type: application/json');
-	print json_encode($result);
+  header('Content-type: application/json');
+  print json_encode($result);
