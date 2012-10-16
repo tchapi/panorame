ef520287a0390818f6ca53625f6c7a20c8f82f0c
tchapi <regbasket@gmail.com>
2012-10-16 21:51:03 +0200

    New deploy script (WIP)

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-# Deployement for tuneefy
+# Deployement script for GitHub
 #!/bin/bash
 
 YELLOW='\033[01;33m'  # bold yellow
@@ -7,106 +7,121 @@ GREEN='\033[01;32m' # green
 BLUE='\033[01;34m'  # blue
 RESET='\033[00;00m' # normal white
 
+APP_BASE_PATH='/home/tchap'
+
+if [ $# -lt 2 ]
+then
+  echo " Usage: $0 (application) (env) [file1 file2 ...]";
+  echo ""
+  exit 1;
+fi  
+
+# Checking application is good
+app=$1;
+
+case $app in
+  "tuneefy") app="tuneefy";;
+  "panorame") app="panorame";;
+  *) echo -e " # "${RED}"ERROR"${RESET}" : Bad application ($app)"
+  echo ""
+  exit 1;;
+esac
+
+# Checking environment is good
+env=$2;
+
+case $env in
+  "prod") env="prod";;
+  "beta") env="beta";;
+  "poc")  env="poc";;
+  *) echo -e " # "${RED}"ERROR"${RESET}" : Bad environment ($env)"
+  echo ""
+  exit 1;;
+esac
+
+date_today=`date '+%Y-%m-%d'`
+timestamp=`date '+%s'`
+DEPLOY_PATH=${APP_BASE_PATH}'/deploy/'${app}
+WWW_PATH=${APP_BASE_PATH}'/www/'${app}'/release-'${date_today}"-"${timestamp}
+WWW_LINK=${APP_BASE_PATH}'/www/'${app}'/'${env}
+
 echo -e ""${RESET}
 echo " # ------------------------- #"
-echo " # panorame promotion script #"
+echo " # "${app}" promotion script #"
 echo " # ------------------------- #"
 echo ""
 
-if [ $# -lt 1 ]
+echo -e " #"${BLUE}" Deployment path : "${RESET}${DEPLOY_PATH}
+echo -e " #"${BLUE}" Release www path : "${RESET}${WWW_PATH}
+echo -e " #"${BLUE}" Live www path : "${RESET}${WWW_LINK}
+echo ""
+
+# Check user
+echo -e " #"${GREEN}" Current user is : "${RESET}$(whoami)
+echo ""
+
+# Go into the deploy directory
+echo -e " #"${GREEN}" Changing directory "${RESET}" to "${DEPLOY_PATH} 
+echo ""
+cd ${DEPLOY_PATH}
+
+# Git all the way
+echo -e " #"${GREEN}" Checking out live branch "${RESET}
+echo ""
+git checkout live
+git reset --hard HEAD
+git pull origin live
+git status
+echo ""
+
+#
+# Do we promote live the whole branch or just files ?
+#
+if [ $# -ge 3 ]
 then
-  echo " Usage: $0 (poc) [file1 file2 ...]";
-  echo ""
-  exit 1;
-fi
 
-# Checking environment is good
-env=$1;
-
-case $env in
-   "poc") real_env="poc";;
-   *) echo -e " # "${RED}"ERROR"${RESET}" : Bad environment ($env)"
-      echo ""
-      exit 1;;
-esac
-
-if [ $# -ge 2 ]
-then
-
-  files=""
-
-  # Checking we have files
+  echo -e " #"${GREEN}" Promoting "${RESET}${env}" environment partially to current release"
+  echo -e " #  "${YELLOW}"\_Files : "
+  
+  # Checking we have files and copying
   for x in "$@"; do 
-    if [ -e $PWD"/"$x ]
+    if [ -e ${DEPLOY_PATH}"/"$x ]
     then
-      files=$files" "$x;
+    echo -e " #   - "$x
+    cp -f ${DEPLOY_PATH}/$x ${WWW_LINK}/.;
     fi
   done
 
-  files=$(sed -e 's/^[[:space:]]*//' <<<"$files")
-
-  echo -e " #"${GREEN}" Promoting "${RESET}${real_env}" environment partially to current release"
-  echo -e " #  "${YELLOW}"\_Files : "${files} 
-
-  # Rsync local
-  echo -e ${BLUE}
-  rsync -RlptgoDvz ${files} -e 'ssh -p 1122' . "tchap@ismerging.us:/home/tchap/www/panorame/" --exclude-from 'exclude.rsync'
   echo -e ${RESET}
-
   echo -e " # "${GREEN}"Done. "${RESET}
   echo ""
 
 else
-  echo -e " #"${GREEN}" Promoting "${RESET}${real_env}" environment to current release"
 
-  echo -e ${BLUE}
-  # Rsync global
-  rsync -rlptgoDvz -e 'ssh -p 1122' . "tchap@ismerging.us:/home/tchap/www/panorame/" --exclude-from 'exclude.rsync'
-  echo -e ${RESET}  
-
-  echo -e " # "${GREEN}"Done."${RESET}
+  echo -e " #"${GREEN}" Promoting "${RESET}${env}" environment to current release"
   echo ""
+  # Copy all files to the destination folder
+  mkdir ${WWW_PATH}
+  rsync -r ${DEPLOY_PATH}/* ${WWW_PATH}/. --exclude-from "${DEPLOY_PATH}/exclude.rsync"
+  
+  echo -e " #"${GREEN}" Linking "${RESET}
+  echo ""
+  # Link
+  unlink ${WWW_LINK}
+  ln -s ${WWW_PATH} ${WWW_LINK}
+
+  echo -e " # "${GREEN}"Done. "${RESET}
+  echo ""
+
 fi
 
-# Building Minified JS
-# while true; do
-#     read -p " # -- Do you wish to build minified Javascript [no] ?" yn
-#     case $yn in
-#         [Yy]* ) echo ""
-#     break;;
-#         [Nn]* ) echo -e " # "${GREEN}"Done. "${RESET}"Exiting."
-#     echo ""
-#     exit;;
-#         * ) echo -e " # "${GREEN}"Done. "${RESET}"Exiting."
-#     echo ""
-#     exit 1;;
-#     esac
-# done
+# Restart Apache
+echo -e " #"${GREEN}" Restarting Apache ... "${RESET}
+echo ""
+sudo /root/apachereload.sh
+echo ""
 
-
-# echo -e " # "${GREEN}"Building"${RESET}" minified JS"
-
-# mv js/min/tuneefy.min.js js/min/tuneefy.min.js.backup
-
-# echo -e ${BLUE}
-# if [ $real_env = "prod" ]
-# then
-#   curl -o js/min/tuneefy.min.js 'http://beta:tuneefy2011@beta.tuneefy.com/admin/minify.php?adv=0&include_build_version=true'
-# else
-#   curl -o js/min/tuneefy.min.js 'http://beta:tuneefy2011@beta.tuneefy.com/admin/minify.php?include_build_version=true&adv=0'
-# fi
-
-# wait $!
-
-# echo -e ${RESET}
-
-# echo -e " # "${GREEN}"Done. "${RESET}
-# echo ""
-
-# echo -e " # "${GREEN}"Updating "${RESET}" minified JS"
-# echo -e ${BLUE}
-# rsync --verbose -RlptgoDvz 'js/min/tuneefy.min.js' -e 'ssh -p 1122' . "tchap@ismerging.us:/home/tchap/www/tuneefy/"${real_env}"/"
+# Done
 echo -e ${RESET}
 echo -e " # "${GREEN}"Done. "${RESET}"Exiting."
 echo ""
-
