59f9562de4f0335d8a3ce68b2044b1c02f481d5f
tchapi <regbasket@gmail.com>
2012-11-24 17:00:22 +0100

    Fix array key name

--- include/controller/constants.php
+++ include/controller/constants.php
@@ -4,7 +4,7 @@ $constants = array(
 
     'frameworks' => array('mapquest', 'gmaps', 'bing', 'nokia', 'openlayers'),
     'providers'  => array('nokia-terrain', 'nokia-road', 'nokia-hybrid', 'bing-road', 'bing-hybrid', 'osmaps', 'mapquest', 'gmaps-terrain', 'gmaps-road', 'gmaps-hybrid'),
-    'engine'     => array('mysql','mongo'),
+    'engines'     => array('mysql','mongo'),
     'pages'      => array(
       array('slug' =>'map', 'name' => 'Home', 'home' => true),
       array('slug' =>'contact', 'name' => 'Contact'),
