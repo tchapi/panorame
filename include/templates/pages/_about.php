cc2ef0654ec9b540a15bb68e30aa7d5b4c1e4616
tchapi <regbasket@gmail.com>
2012-11-25 21:30:46 +0100

    New static pages and some style

--- include/templates/pages/_about.php
+++ include/templates/pages/_about.php
@@ -1,31 +1,68 @@
 <div id="top" class="about">
   <div class="hero-unit">
     <h1>Panora.me</h1>
-    <h2>Accurate distance-search for maniacs</h2>
+    <h2>The city around you. Visual. Dynamic. Reinvented.</h2>
     <p><a class="btn btn-success btn-large" href="#"><span class="lsf">playmedia</span> Watch the tour</a></p>
   </div>
 </div>
 <div id="paragraphs" class="about">
   <div class="inside">
 
-  <h3>The standard Lorem Ipsum passage, used since the 1500s</h3>
+  <h3>Vision</h3>
 
-  <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+  <p>Panorame redefines the 'search-for-a-place-to-go' experience.</p>
 
-  <h3>Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC</h3>
+  <p>You were exploring long webpages of nice places, and then only
+  estimating if it's not too far from home? How often did you complain
+  that your best choices were in fact too far, that you had just lost
+  5minutes, and you needed to restart the search from scratch?
+  That's really not the experience the web should offer you.</p>
 
-  <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+  <p>Our research shows that 94% of users are regularly irritated by the
+  classical 'search-for-a-place' experience on the web.</p>
 
-  <h3>1914 translation by H. Rackham</h3>
+  <p>What if the area easily reachable around your home mattered first.
+  What if you were intuitively and dynamically configuring it,
+  visualizing it. What if the search-for-a-place part came only then, in
+  your personalized area, your playzone.</p>
 
-  <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
+  <p>What if, from the very beginning, we took into consideration that you
+  were willing to move not farther than 10min by foot from home? Or
+  maybe by bike today. And why not tomorrow with combining walking and
+  taking the tube.</p>
 
-  <h3>Section 1.10.33 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC</h3>
+  <p>With Panorame you first define your start location (home is a good
+  choice!), choose your transportation mean, and decide how long you are
+  willing to invest in the journey. Here comes your playzone: you just
+  defined it, and it is there, on your screen. Of course you can refine
+  your parameters, your playzone is totally dynamic and will update
+  instantly.
+  Then only come the landmarks recommendations, in the area covered by
+  your zone of course.</p>
 
-  <p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."</p>
+  <h3>Technology</h3>
 
-  <h3>1914 translation by H. Rackham</h3>
+  <div class="row-fluid icons">
+    <div class="span4 iconHolder"><div class="lsf bigIcon">map</div>Maps providers agnostic</div>
+    <div class="span4 iconHolder"><div class="lsf bigIcon">dashboard</div>Real-time (doesn't it feel so ?)</div>
+    <div class="span4 iconHolder"><div class="lsf bigIcon">cloud</div>Open (see API)</div>
+  </div>
 
-  <p>"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."</p>
+  <h3>Fair use / ToS</h3>
+
+  <p>Using Panorame is free and will stay so: if you find it useful and ergonomic, we are happy. If not, we are happy to discuss the reasons and to try to improve the service.</p>
+
+  <p>We have a lot of ideas about how the Panorame technology could benefit other websites and services. If you are interested in discussing it, please feel free to contact us at the-team-is-listening@panorame.fr. You can also visit the API section.</p>
+
+  <h3>Hardcopy posters</h3>
+
+  <p>Panorame offers you the possibility to order a customized poster of your playzone. To be pinned in your toilets. Or elsewhere.
+
+  <p>Your friend just moved in a new flat: isn't it the perfect gift?</p>
+
+  <p>Your playzone hardcopy is delivered with a pile of stylized post-its that you could use to enrich the poster each time you discover a new cool place around. That's your playzone, your neighboryouds.</p>
+
+  <p><button class="btn btn-info">Order now (19,99€)</button></p>
+
   </div>
 </div>
