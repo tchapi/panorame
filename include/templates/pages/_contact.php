5a564458927b107548bbd2436084a63cb0cb92d8
Eric <eric.boniface@yahoo.fr>
2013-01-09 09:57:42 +0100

    Finetuning des textes des pages static (contact)

--- include/templates/pages/_contact.php
+++ include/templates/pages/_contact.php
@@ -18,11 +18,28 @@
 
   <h3>Partnerships</h3>
 
-  <p>Your job is to select nices places and recommend it to your visitors? Give them an easier, smoother, simply more natural experience of your service: upgrade your 'search-for-a-place' ergonomics by embedding Panorame in your website. (--> see API)</p>
-
-  <p>You manage a cultural or entertainment place, and would like it to appear in the playzones of Panorame users ? (contact us) </p>
-
-  <p>You edit opendata sets from your organization ? These could be part of choosable data for Panorame users. (contact us) </p>
+  <p>
+    <ul>
+      <li>Your job is to curate places / landmarks and recommend it to your visitors? Give them an easier, smoother, simply more natural experience of your service: upgrade your 'search-for-a-place' ergonomics by embedding Panorame in your website.
+(--> see API)</li>
+      <li>You manage a cultural or entertainment place, and would like it to appear in the playzones of Panorame users?
+(--> contact us)</li>
+      <li>You edit/administrate opendata sets from your organization? These could be part of printable data for Panorame users.
+(--> contact us)</li>
+    </ul>
+  </p>
+  
+  <h3>API</h3>
+  
+  <p>Panorame offers an API which allow developers to embed the Panorame experience into their web services.</p>
+  
+  <p>For web services dedicated to recommending places, landmarks, addresses, it contributes to significantly enhance the end user experience. It allows the user to focus the search on his Panorame playzone, to make it very visual and dynamic.</p>
+  
+  <p>The Panorame API includes:
+      - the Playzone definition and visualization module;
+      - a function to query if a location is part of the current playzone;
+      - customizable search fields.
+  </p>
 
   </div>
 </div>
